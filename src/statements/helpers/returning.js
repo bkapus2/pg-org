0eba436dee00994991b3d18f6a255c20b5c9cacb
Unknown <brian.kupi@gmail.com>
2018-04-07 22:31:08 -0500

    added a returning statement/test

--- src/statements/helpers/returning.js
+++ src/statements/helpers/returning.js
@@ -1,3 +1,8 @@
-export default function returning(returning) {
-  
+import required from '@/utils/required';
+
+export default function select(params) {
+  const {
+    columns=required('columns'),
+  } = params;
+  return `RETURNING\n\t${columns.join(',\n\t')}`;
 }
--- test/statements/helpers/returning.js
+++ test/statements/helpers/returning.js
@@ -1,6 +1,22 @@
 /* global it, describe */
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
+import returning from '@/statements/helpers/returning';
 
 export default function() {
-  
+  describe('statements/helpers/returning', ()=>{
+    const columns = ['col_a', 'col_b'];
+
+    it('should be a function', () => {
+      expect(returning).to.be.a('function');
+    });
+        
+    it('should require a columns parameter', () => {
+      expect(() => returning({})).to.throw(Error, '\'columns\' is a required parameter');
+    });
+
+    it('should return a returning statement', () => {
+      const value = returning({ columns });
+      expect(value).to.be.equal('RETURNING\n\tcol_a,\n\tcol_b');
+    });
+  });
 }
