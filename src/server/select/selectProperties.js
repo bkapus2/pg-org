c45330facec123dd4971cbcee9cf191019955195
Unknown <brian.kupi@gmail.com>
2018-04-21 17:37:40 -0500

    Resolved other merge conflict

--- src/server/select/selectProperties.js
+++ src/server/select/selectProperties.js
@@ -5,28 +5,6 @@ export default function selectProperties({ query, model, queryHandler, selectPro
   const { mode, distinct } = options;
   const { tableName, objectMap, arrayMap } = model;
   return new Promise((resolve, reject) => {
-<<<<<<< Updated upstream
-    whereResolver(model, query)
-      .then((where) => {
-        const text = `
-          SELECT${distinct ? ' DISTINCT' : '' } ${selectProps.join(', ')}
-          FROM ${tableName}
-          ${ where ? 'WHERE ' + where : '' };`;
-        // console.log(text);
-    
-        function mapResults(results) {
-          if (mode === 'object') {
-            return objectMap(results);
-          } else {
-            return arrayMap(results);
-          }
-        }
-    
-        return queryHandler({ text, rowMode: 'array' })
-          .then(mapResults)
-          .then(resolve);
-      })
-=======
     const where = whereStatement(query, propModels);
     const clauses = [
       `SELECT${distinct ? ' DISTINCT' : '' } ${selectProps.join(', ')} FROM ${tableName}`,
@@ -45,7 +23,6 @@ export default function selectProperties({ query, model, queryHandler, selectPro
     queryHandler({ text, rowMode: 'array' })
       .then(mapResults)
       .then(resolve)
->>>>>>> Stashed changes
       .catch(reject);
   });
 }
