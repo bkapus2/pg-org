90ec49b9a2da2461889d17c40ff82c9df7698a39
Unknown <brian.kupi@gmail.com>
2018-07-04 11:16:59 -0500

    Added more conditions to inset tests

--- src/server/blackboxTests/insert.spec.js
+++ src/server/blackboxTests/insert.spec.js
@@ -1,7 +1,7 @@
 /* global describe, before, afterEach, it */
 import { expect } from 'chai';
 import clearTables from '../testUtils/clearTables';
-import { emails } from '../testUtils/tables';
+import { users, emails } from '../testUtils/tables';
 // import executeQuery from '../testUtils/executeQuery';
 
 export default function() {
@@ -18,8 +18,32 @@ export default function() {
       expect(result).to.have.lengthOf(1);
       expect(result[0]).to.have.property('email');
       expect(result[0].email).to.be.equal('brian.kupi@gmail.com');
-      expect(result[0]).to.have.property('id');
-      expect(result[0].id).to.be.a('number');
     });
+
+    it('should allow complex insertes (related records)', async function() {
+      const result = await users.insert([
+        {
+          firstName: 'Brian',
+          lastName: 'Kapustka',
+          emails: [
+            {
+              email: 'brian.kupi@gmail.com',
+            },
+          ],
+        }
+      ]);
+      expect(result).to.have.lengthOf(1);
+      expect(result[0]).to.have.property('firstName');
+      expect(result[0].firstName).to.be.equal('Brian');
+
+      expect(result[0]).to.have.property('lastName');
+      expect(result[0].lastName).to.be.equal('Kapustka');
+
+      expect(result[0]).to.have.property('emails');
+      expect(result[0].emails).to.have.lengthOf(1);
+      expect(result[0].emails[0]).to.have.property('email');
+      expect(result[0].emails[0].email).to.be.equal('brian.kupi@gmail.com');
+      expect(result[0].emails[0].userId).to.be.equal(result[0].id);
+    })
   });
 }
